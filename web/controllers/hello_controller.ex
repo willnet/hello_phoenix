fe8a96b3eac342214b1c5773cc4035ec008aaa40
willnet <netwillnet@gmail.com>
2015-10-12 17:33:20 +0900

    Change from controller and view section

--- web/controllers/hello_controller.ex
+++ web/controllers/hello_controller.ex
@@ -2,7 +2,11 @@ defmodule HelloPhoenix.HelloController do
   use HelloPhoenix.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    conn
+    |> put_flash(:info, "Welcome to Phoenix, from flash info!")
+    |> put_flash(:error, "Let's pretend we have an error.")
+    |> render("index.html")
+    # render conn, "index.html"
   end
 
   def show(conn, %{"messenger" => messenger}) do
